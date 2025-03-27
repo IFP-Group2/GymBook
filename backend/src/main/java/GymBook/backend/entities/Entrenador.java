55dbfaf864f8093062b512e42a6fca409399fae1
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-03-27 12:26:39 +0100

    entidad entrenador completa

--- backend/src/main/java/GymBook/backend/entities/Entrenador.java
+++ backend/src/main/java/GymBook/backend/entities/Entrenador.java
@@ -1,4 +1,61 @@
 package GymBook.backend.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "entrenador")
 public class Entrenador {
+    @Id
+    private Long usuarioId;
+    private String especialidad;
+    private int experiencia;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    public Entrenador() {
+    }
+
+    public Entrenador(Long usuarioId, String especialidad, int experiencia, Usuario usuario) {
+        this.usuarioId = usuarioId;
+        this.especialidad = especialidad;
+        this.experiencia = experiencia;
+        this.usuario = usuario;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
