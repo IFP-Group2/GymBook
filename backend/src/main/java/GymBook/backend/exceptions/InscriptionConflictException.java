5b970b814308463a11758234e72f962a557f14de
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-15 16:20:05 +0200

    InscriptionConflictException creada

---
+++ backend/src/main/java/GymBook/backend/exceptions/InscriptionConflictException.java
@@ -0,0 +1,8 @@
+package GymBook.backend.exceptions;
+
+public class InscriptionConflictException extends RuntimeException {
+    public InscriptionConflictException(String message) {
+        super(message);
+    }
+
+}
