8172e47e2d0c1d723056faa52372a80079ea537e
nonim12 <ytanonim12@gmail.com>
2025-04-22 10:30:42 +0200

    Realizo  comentarios de la pagina ForgotPasswordPage.js

--- frontend/src/pages/ForgotPasswordPage.js
+++ frontend/src/pages/ForgotPasswordPage.js
@@ -1,41 +1,43 @@
 import React, { useState } from 'react'; import axios from 'axios'; import '../styles/ForgotPasswordPage.css';
-
-const ForgotPasswordPage = () => { const [email, setEmail] = useState(''); const [message, setMessage] = useState('');
+//Funcion que contiene la logica de ForgotPasswordPage
+const ForgotPasswordPage = () => {
+    const [email, setEmail] = useState(''); const [message, setMessage] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (!email) {
-            setMessage('Por favor, ingresa tu correo electrónico.');
+            setMessage('Por favor, ingresa tu correo electrónico.');//Mensaje si no se rellena el campo email
             return;
         }
-    
+        //Bloque try-catch destinado al correo de recuperacion
         try {
             const response = await axios.post('http://localhost:8080/auth/forgot-password', {
                 email,
             });
-            setMessage('Revisa tu correo para instrucciones de recuperación.');
+            setMessage('Revisa tu correo para instrucciones de recuperación.');//Mensaje de exito
         } catch (error) {
+            //Mensaje de error si no se puede enviar el correo de recuperacion
             setMessage('Error al enviar el correo de recuperación.');
         }
-    
+
         setEmail('');
     };
-
-return (
-    <div className="forgot-password-container">
-        <h1>Recuperar Contraseña</h1>
-        <form onSubmit={handleSubmit} className="forgot-password-form">
-            <input
-                type="email"
-                placeholder="Correo electrónico"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <button type="submit">Enviar</button>
-        </form>
-        {message && <p className="message">{message}</p>}
-    </div>
-);
+    //Apariencia de ForgotPasswordPage
+    return (
+        <div className="forgot-password-container">
+            <h1>Recuperar Contraseña</h1>
+            <form onSubmit={handleSubmit} className="forgot-password-form">
+                <input
+                    type="email"
+                    placeholder="Correo electrónico"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button type="submit">Enviar</button>
+            </form>
+            {message && <p className="message">{message}</p>}
+        </div>
+    );
 };
 
 export default ForgotPasswordPage;
